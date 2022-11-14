c9bdfb0db8ad3d0c0d7e52d52030ded36c52c959
eduardo.cardoso <eduardo.cardoso.ext@synchro.com.br>
2022-11-14 20:26:11 -0300

    Primeiro push

---
+++ src/main/java/com/jogamais/ufcg/UfcgApplication.java
@@ -0,0 +1,13 @@
+package com.jogamais.ufcg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UfcgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UfcgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jogamais/ufcg/UfcgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jogamais.ufcg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UfcgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
