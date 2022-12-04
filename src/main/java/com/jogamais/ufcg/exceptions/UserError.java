845c2cb6564b965d331591834fda78e3b9bf6d9c
Adriano <adriano.junior@ccc.ufcg.edu.br>
2022-12-04 18:49:52 -0300

    Refactor: InternalUserError to UserEror

--- src/main/java/com/jogamais/ufcg/exceptions/UserError.java
+++ src/main/java/com/jogamais/ufcg/exceptions/UserError.java
@@ -1,21 +1,19 @@
 package com.jogamais.ufcg.exceptions;
 
-import com.jogamais.ufcg.models.User;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-public class InternalUserError {
+public class UserError {
 
     static final String USER_NOT_EXIST = "O usuário não cadastrado.";
 
     static final String USER_ALREADY_EXIST = "O usuário %s já está cadastrado com o CPF: %s";
 
     public static ResponseEntity<CustomTypeError> errorUserNotExist() {
-        return new ResponseEntity<CustomTypeError>(new CustomTypeError(InternalUserError.USER_NOT_EXIST), HttpStatus.NOT_FOUND);
+        return new ResponseEntity<CustomTypeError>(new CustomTypeError(UserError.USER_NOT_EXIST), HttpStatus.NOT_FOUND);
     }
 
-    //é necessário que tenha o model de cliente
     public static ResponseEntity<CustomTypeError> errorUserAlreadyExist(String cpf) {
-        return new ResponseEntity<CustomTypeError>(new CustomTypeError(String.format(InternalUserError.USER_ALREADY_EXIST, cpf)), HttpStatus.CONFLICT);
+        return new ResponseEntity<CustomTypeError>(new CustomTypeError(String.format(UserError.USER_ALREADY_EXIST, cpf)), HttpStatus.CONFLICT);
     }
 }
