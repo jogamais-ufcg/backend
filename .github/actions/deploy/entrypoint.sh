80fdf181f0cb18b4848aa83f9d10d80167ad1ad5
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2022-12-10 19:41:58 -0300

    fix: remove sudo command

--- .github/actions/deploy/entrypoint.sh
+++ .github/actions/deploy/entrypoint.sh
@@ -3,11 +3,11 @@
 # Install OpenJDK 18
 wget https://download.java.net/java/GA/jdk18/43f95e8614114aeaa8e8a5fcf20a682d/36/GPL/openjdk-18_linux-x64_bin.tar.gz
 tar -xvf openjdk-18_linux-x64_bin.tar.gz
-sudo mv jdk-18* /opt/
+mv jdk-18* /opt/
 export JAVA_HOME=/opt/jdk-18
 export PATH=$PATH:$JAVA_HOME/bin
-sudo update-alternatives --install "/usr/bin/java" "java" "/opt/jdk-18/bin/java" 1
-sudo update-alternatives --install "/usr/bin/javac" "javac" "/opt/jdk-18/bin/javac" 1
+update-alternatives --install "/usr/bin/java" "java" "/opt/jdk-18/bin/java" 1
+update-alternatives --install "/usr/bin/javac" "javac" "/opt/jdk-18/bin/javac" 1
 
 # Save key file
 echo "$1" >> key.pem
