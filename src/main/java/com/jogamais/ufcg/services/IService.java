65dc76dac0c9027cc28b573018c948c879977950
eduardo.cardoso <eduardo.cardoso.ext@synchro.com.br>
2022-12-12 19:15:40 -0300

    feat: Implementing search method on IService

--- src/main/java/com/jogamais/ufcg/services/IService.java
+++ src/main/java/com/jogamais/ufcg/services/IService.java
@@ -1,17 +1,19 @@
 package com.jogamais.ufcg.services;
 
 import com.jogamais.ufcg.exceptions.UserException;
-import com.jogamais.ufcg.models.User;
+import org.springframework.data.domain.Page;
 
 import java.util.List;
 
 public interface IService<T> {
 
-	public abstract Object getById(Long id) throws UserException;
+    public abstract Object getById(Long id) throws UserException;
 
-	public abstract T create(T entity) throws UserException;
+    public abstract T create(T entity) throws UserException;
 
-	public abstract void deleteById(Long id) throws UserException;
+    public abstract void deleteById(Long id) throws UserException;
 
-	public abstract List<T> findAll();
+    public abstract List<T> findAll();
+
+    public abstract Page<T> search(String searchTerm, int page, int size);
 }
