ecfca20b08ac1903fab931e539f8fe72cbff513e
Guilherme Aureliano <49173189+GuilhermeAureliano@users.noreply.github.com>
2022-12-28 16:16:26 -0300

    feat: add models and dto from Appointment

---
+++ src/main/java/com/jogamais/ufcg/models/AdminAppointment.java
@@ -0,0 +1,36 @@
+package com.jogamais.ufcg.models;
+
+import java.util.Date;
+
+import com.jogamais.ufcg.models.pk.AppointmentPK;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "ADMIN_APPOINTMENT")
+public class AdminAppointment {
+
+    @EmbeddedId
+    private AppointmentPK id;
+
+    @Column(name = "USER_NAME", nullable = false)
+    private String userName;
+
+    @Column(name = "USER_EMAIL", nullable = false)
+    private String userEmail;
+
+    @Column(name = "USER_PHONE", nullable = false)
+    private String userPhone;
+
+    @Column(name = "APPOINTMENT_DATE", nullable = false)
+    private Date appointmentDate;
+
+    @Column(name = "PLAYER_LIST", nullable = false)
+    private String playerList;
+
+}
---
+++ src/main/java/com/jogamais/ufcg/models/UserAppointment.java
@@ -0,0 +1,25 @@
+package com.jogamais.ufcg.models;
+
+import java.util.Date;
+
+import com.jogamais.ufcg.models.pk.AppointmentPK;
+import lombok.*;
+
+import javax.persistence.*;
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "USER_APPOINTMENT")
+public class UserAppointment {
+
+    @EmbeddedId
+    private AppointmentPK id;
+
+    @Column(name = "APPOINTMENT_DATE", nullable = false)
+    private Date appointmentDate;
+
+    @Column(name = "PLAYER_LIST", nullable = false)
+    private String playerList;
+}
